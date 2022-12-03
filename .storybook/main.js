c92f903b005a974dd5dd93b578936abf7f74af21
Patrick Jones <forksofpower@gmail.com>
2022-12-03 12:33:21 -0600

    add Button example

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  stories: [
+  "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  framework: "@storybook/react",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5",
+    
+  },
   webpackFinal: async (config, { configType }) => {
     // Make whatever fine-grained changes you need
     config.module.rules.push({
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,24 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { Story } from "@storybook/react";
+import { Button, ButtonProps } from "../components/Button";
 
-import { Button } from './Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+};
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  children: "Primary",
+  variant: "primary",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Danger = Template.bind({});
+Danger.args = {
+  children: "Danger",
+  variant: "danger",
+  shape: "rounded",
 };
